e71daee3cb69568cd9e5838400ad1acb315d4d59
Hernan Y.Ke <yuriqiao@gmail.com>
2016-01-24 20:10:32 -0500

    Add .gitignore

---
+++ L4_5.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hernan Y.Ke
+ * Date: 2016/1/24
+ * Time: 20:06
+ */
+//print cannot print more than 1 param.
+//echo is slightly faster
+
+print("long......\n");
+echo "a","b",",","c";
