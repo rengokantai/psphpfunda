7f88578f046ccc99aa58c5ea7a3b835f986df66c
Hernan Y.Ke <yuriqiao@gmail.com>
2016-01-26 22:57:02 -0500

    strpos

---
+++ L4_6.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hernan Y.Ke
+ * Date: 2016/1/24
+ * Time: 20:12
+ */
+//strtolower,strtoupper
+$var = "low";
+echo strtoupper($var);
+echo strlen($var);
---
+++ L4_8.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hernan Y.Ke
+ * Date: 2016/1/26
+ * Time: 22:37
+ */
+//strpos
+echo strpos("longstringlongstring","string",7);
