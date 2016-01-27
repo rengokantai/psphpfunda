0be0e0c36c427ed44258bcbe352fccc5970a7f23
Hernan Y.Ke <yuriqiao@gmail.com>
2016-01-26 23:07:32 -0500

    str_replace

---
+++ L4_9.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hernan Y.Ke
+ * Date: 2016/1/26
+ * Time: 22:57
+ */
+//str_replace
+
+$newstring =  str_replace("long","short", "longstring", $count);
+echo $count;
